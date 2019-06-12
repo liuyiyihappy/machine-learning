15c0d920306b4608981aa7828341fde8f7617de0
liuyiyihappy <195289532@qq.com>
2019-06-12 18:02:43 +0800

    0612

---
+++ kNN.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[10]:
+
+
+from numpy import *
+import operator
+
+def createDataSet():
+    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    lables = ['A','A','B','B']
+    return group,lables
+
